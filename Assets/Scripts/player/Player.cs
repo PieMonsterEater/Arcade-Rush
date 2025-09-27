ce5fa4b05bfcbdcb74e4ea30169609fb755c8091
fishstick1234 <jamesclute95@gmail.com>
2025-09-27 18:37:58 -0400

    Added player Animations.

--- Assets/Scripts/player/Player.cs
+++ Assets/Scripts/player/Player.cs
@@ -3,6 +3,8 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
+    bool isMoving = false;
+    Animator animator;
     private Vector2 moveDir = new Vector2(0, 0);
     [SerializeField] private float moveSpeed = 5;
     [SerializeField] private Rigidbody2D rb = null;
@@ -11,13 +13,21 @@ public class Player : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+      animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.linearVelocity = moveDir * moveSpeed;
+        animator.SetBool("Moving", isMoving);
+        if (((moveDir.x <= 0.01f && moveDir.x >= -0.01f) || (moveDir.x >= 0.01f && moveDir.x <= -0.01f)) && (moveDir.y <= 0.01f && moveDir.y >= -0.01f) || (moveDir.y >= 0.01f && moveDir.y <= -0.01f))
+        {
+            isMoving = false;
+        }
+        else {
+            isMoving = true;
+        }
+            rb.linearVelocity = moveDir * moveSpeed;
     }
 
     public void OnMove(InputAction.CallbackContext context)
