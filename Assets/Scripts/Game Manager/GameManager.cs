5f12aab65becf6749303240bf04023be6b29e7d1
pie <pie@piescode.net>
2025-09-27 23:17:40 -0400

    More game loop stuff

--- Assets/Scripts/Game Manager/GameManager.cs
+++ Assets/Scripts/Game Manager/GameManager.cs
@@ -7,17 +7,21 @@ public class GameManager : MonoBehaviour
     private int numBrokenMachines = 0;
     private int score = 0;
     private float timeRemaining = 120;
+    private float roundStartTimer = 3;
+    private float roundEndTimer = 3;
     private int displayMinutes = 0;
     private int displaySeconds = 0;
     public int Round { get; private set; }
 
     public Player player;
+    private kid_movement[] kids;
     public bool isStarting { get; private set; }
     public bool isEnding { get; private set; }
 
     [SerializeField] TMP_Text timeDisplay;
     [SerializeField] TMP_Text scoreDisplay;
     [SerializeField] TMP_Text brokenDisplay;
+    [SerializeField] TMP_Text roundDisplay;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -25,7 +29,7 @@ public class GameManager : MonoBehaviour
         isStarting = true;
         isEnding = false;
         Round = 1;
-        
+
         displayMinutes = (int)(timeRemaining / 60);
         displaySeconds = (int)(timeRemaining % 60);
 
@@ -43,14 +47,52 @@ public class GameManager : MonoBehaviour
             if (!machineArray[toBreak].IsBroken()) machineArray[toBreak].DestroyMachine();
             else i--;
         }
+
+        player = UnityEngine.Object.FindAnyObjectByType<Player>();
+        kids = UnityEngine.Object.FindObjectsByType<kid_movement>(
+            FindObjectsInactive.Exclude,
+            FindObjectsSortMode.None
+        );
     }
 
     public void StartRound()
-    { 
-        
+    {
+        roundStartTimer -= Time.deltaTime;
+        roundDisplay.SetActive(true);
+        roundDisplay.text = "Round " + Round;
+
+        if (roundStartTimer <= 0)
+        {
+            isStarting = false;
+            roundDisplay.SetActive(false);
+            roundStartTimer = 3;
+        }
     }
 
-    
+    private void EndRound()
+    {
+        roundEndTimer -= Time.deltaTime;
+
+        if (roundEndTimer <= 0)
+        {
+            isEnding = false;
+            foreach (kid_movement kid in kids)
+            {
+                kid.gameObject.transform = kid.startPos;
+                kid.UpdateSpeed(1);
+            }
+            Round++;
+            isStarting = true;
+            roundEndTimer = 3;
+        }
+    }
+
+    private void GameOver()
+    {
+        roundDisplay.text = "Game Over";
+        roundDisplay.SetActive(true);
+    }
+
 
     // Update is called once per frame
     void FixedUpdate()
@@ -81,7 +123,9 @@ public class GameManager : MonoBehaviour
         numBrokenMachines += change;
         if (numBrokenMachines <= 0)
         {
-            Round++;
+            isEnding = true;
         }
     }
+
+    public void AddScore(int change) => score += change;
 } 
--- Assets/Scripts/kid/kid_movement.cs
+++ Assets/Scripts/kid/kid_movement.cs
@@ -2,37 +2,40 @@ using UnityEngine;
 using System.Collections.Generic;
 
 public class kid_movement : MonoBehaviour
-
-
 {
-     private Vector2 moveDir = new Vector2(0, 0);
-    [SerializeField] private float moveSpeed = 8;
+    [SerializeField] private float moveSpeed = 6;
     [SerializeField] private Rigidbody2D rb = null;
+
+    private Transform startPos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Awake()
     {
-        rb.linearVelocity = new Vector2(Random.Range(-5, 5), Random.Range(-5,5)).normalized * moveSpeed;
+        rb.linearVelocity = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5)).normalized * moveSpeed;
+        startPos = this.gameObject.transform;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Random.Range(1,100)==99)
+        if (Random.Range(1, 100) == 99)
         {
-            rb.linearVelocity = new Vector2(Random.Range(-5, 5), Random.Range(-5,5)).normalized * moveSpeed;
+            rb.linearVelocity = new Vector2(Random.Range(-5, 5), Random.Range(-5, 5)).normalized * moveSpeed;
         }
-        
+
         /*var unispeed = new Vector2(Random.Range(0, 5), Random.Range(0,5)).Normalize();*/
 
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
     {
-        if(collision.gameObject.name.StartsWith("ArcadeMachine")) {
+        if (collision.gameObject.name.StartsWith("ArcadeMachine"))
+        {
             ArcadeMachine machine = collision.gameObject.GetComponent<ArcadeMachine>();
             machine.DestroyMachine();
         }
 
         //rb.linearVelocity = new Vector2(Random.Range(-5, 5), Random.Range(-5,5)).normalized * moveSpeed;        
     }
+
+    public void UpdateSpeed(float change) => moveSpeed += change;
 }
